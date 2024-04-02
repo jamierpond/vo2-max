c8096f8e5620a0249756f3461510675434c7e48a
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2024-04-01 22:01:13 -0700

    on click instead

--- app/page.tsx
+++ app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
     }
   }, [frameCount, isHighIntensity]);
 
-  const handleTap = () => {
+  const handeClick = () => {
     setIsPaused((prevIsPaused) => !prevIsPaused);
   };
 
@@ -133,7 +133,7 @@ export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-center p-8"
-      onTouchStart={handleTap}
+      onClick={handeClick}
     >
       <div className="flex flex-col items-center">
         <Image
